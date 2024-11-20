9d5c07642842cc08535a84f9f59fdcbed354339b
Devon Daviau <devondaviau@yahoo.ca>
2024-11-20 17:10:35 -0500

    set up persisting updated invoices to the database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -32,3 +32,24 @@ export async function createInvoice(formData: FormData) {
   expirePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  expirePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
